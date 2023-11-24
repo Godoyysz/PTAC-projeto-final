20788e755f866a65987aab891e41f742d32910b7
Godoyysz <131817536+Godoyysz@users.noreply.github.com>
2023-11-24 17:32:17 -0400

    commit
    baixei o node modules
    criei a navbar com o bootstrap

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,9 @@
+import navbar from "./componentes/navbar";
+
 export default function Home(){
     return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
+       <div>
+        <navbar/>
+       </div>
      );
 }
---
+++ src/componentes/navbar.jsx
@@ -0,0 +1,14 @@
+export default function navbar(){
+    return(
+        <div>
+
+       
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+    </nav>
+
+       </div>
+     );
+}
